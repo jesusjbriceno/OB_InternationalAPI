3bdcf74e03bd4cde32c82fa360ce757fbda0eb90
Jesús Briceño <ineco.jjbm@externomf.es>
2023-03-01 16:56:04 +0100

    Implement Internationalization and globalization

---
+++ InternationalAPI/Controllers/PostsController.cs
@@ -0,0 +1,54 @@
+﻿using InternationalAPI.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
+
+namespace InternationalAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostsController : ControllerBase
+    {
+        private readonly IStringLocalizer<PostsController> _stringLocalizer;
+        private readonly IStringLocalizer<SharedResource> _sharedResourceLocalizer;
+
+
+        public PostsController(IStringLocalizer<PostsController> stringLocalizer, IStringLocalizer<SharedResource> sharedResourceLocalizer)
+        {
+            _stringLocalizer = stringLocalizer;
+            _sharedResourceLocalizer = sharedResourceLocalizer;
+        }
+
+        [HttpGet]
+        [Route("PostControllerResource")]
+        public IActionResult GetUsingPostControllerResource()
+        {
+            // Find text
+            var article = _stringLocalizer["Article"];
+            var postName = _stringLocalizer.GetString("Welcome").Value ?? string.Empty;
+
+            return Ok(new             {
+                PostType = article.Value,
+                PotsName = postName
+            });
+        }
+
+        [HttpGet]
+        [Route("SharedResource")]
+        public IActionResult GetUsingSharedResource()
+        {
+            // Find text
+            var article = _stringLocalizer["Article"];
+            var postName = _stringLocalizer.GetString("Welcome").Value ?? string.Empty;
+            var todayIs = string.Format(_sharedResourceLocalizer.GetString("TodayIs"), DateTime.Now.ToLongDateString());
+
+            return Ok(new
+            {
+                PostType = article.Value,
+                PotsName = postName,
+                TodayIs = todayIs
+            });
+        }
+
+    }
+}
--- InternationalAPI/Program.cs
+++ InternationalAPI/Program.cs
@@ -1,6 +1,10 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+// 1. LOCALIZATION
+builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
+
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -8,6 +12,19 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+
+// 2. SUPPORTED CULTURES
+var suportedCutltures = new[] { "en-US", "es-ES", "fr-FR" }; // USA English, Spain spanish, Frnace french
+var localizationOptions = new RequestLocalizationOptions()
+    .SetDefaultCulture(suportedCutltures[0])        // English by default
+    .AddSupportedCultures(suportedCutltures)        // Add all supported cultures
+    .AddSupportedUICultures(suportedCutltures);     // Add supported cultures to UI
+
+
+// 3. ADD LOCALIZATION TO APP
+app.UseRequestLocalization(localizationOptions);
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
